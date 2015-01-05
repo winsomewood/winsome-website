dc5c53b54f6f52439b70ed821515d74b82f5c77e
golf <golf@twitch.tv>
2015-01-05 03:24:00 -0800

    fix float bug

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -4,7 +4,7 @@ module ItemsHelper
   end
 
   def display_dimensions_metric(item)
-    "#{(item.length.to_i * 2.54).round(2)} cm x #{(item.width.to_i * 2.54).round(2)} cm x #{(item.height.to_i * 2.54).round(2)} cm"
+    "#{(item.length.to_f * 2.54).round(2)} cm x #{(item.width.to_f * 2.54).round(2)} cm x #{(item.height.to_f * 2.54).round(2)} cm"
   end
 
 end
