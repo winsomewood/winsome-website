9646a88f0518f016b8f9db17b74e6300a0eaed88
golf <golf@twitch.tv>
2015-01-05 20:32:16 -0800

    fix ssl email bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :port => 587
+    :port => 587,
+    :enable_starttls_auto => false
   }
 
   # Print deprecation notices to the Rails logger.
