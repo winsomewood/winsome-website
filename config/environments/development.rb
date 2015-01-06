3bdad298cafe622a5f1d10a41804e12b765f452b
golf <golf@twitch.tv>
2015-01-05 20:11:49 -0800

    test this email hotfix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,12 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+
+  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
