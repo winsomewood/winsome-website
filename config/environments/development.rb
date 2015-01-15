17884490451f4607dcbc3445305cfb4d3dcb25b3
golf <golf@twitch.tv>
2015-01-14 18:10:00 -0800

    fix production emnails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,12 +15,7 @@ Rails.application.configure do
 
 
   # config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    :port => 587,
-    :enable_starttls_auto => false
-  }
+  config.action_mailer.perform_deliveries = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
