0930aba2044768022dc0aa51ce76a6c31ff4a093
Golf Sinteppadon <golf@twitch.tv>
2015-09-07 15:09:04 -0700

    css changes

--- app/models/replacement.rb
+++ app/models/replacement.rb
@@ -8,23 +8,27 @@ class Replacement < ActiveType::Object
   attribute :zip, :text
   attribute :phone, :text
   attribute :email, :text
+
   attribute :purchased_at, :text
   attribute :retailer, :text
   attribute :controlno, :text
   attribute :itemno, :text
   attribute :description, :text
+
   attribute :partno1, :text
   attribute :quantity1, :text
   attribute :partname1, :text
   attribute :reason1, :text
+
   attribute :partno2, :text
-  attribute :quantity2, :text
   attribute :partname2, :text
+  attribute :quantity2, :text
   attribute :reason2, :text
   attribute :partno3, :text
-  attribute :quantity3, :text
   attribute :partname3, :text
+  attribute :quantity3, :text
   attribute :reason3, :text
+
 
   validates :name, presence: true
   validates :address1, presence: true
@@ -38,9 +42,10 @@ class Replacement < ActiveType::Object
   validates :controlno, presence: true
   validates :itemno, presence: true
   validates :description, presence: true
+
   validates :partno1, presence: true
-  validates :quantity1, presence: true
   validates :partname1, presence: true
+  validates :quantity1, presence: true
   validates :reason1, presence: true
 
   after_save lambda { InfoMailer.replacement_email(self).deliver_now }
