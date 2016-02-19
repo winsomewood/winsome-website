89cce7676c02e02aa7d27ff488af83101ce8303f
Golf Sinteppadon <golf@twitch.tv>
2016-02-18 23:00:25 -0500

    + Rows button in replacements page

--- app/assets/javascripts/replacements.js
+++ app/assets/javascripts/replacements.js
@@ -1,31 +1,45 @@
-// $(function () {
-//   if ($('.js-add-rows').length) {
-//     $('.js-add-rows').on('click', function (e) {
-//       e.preventDefault();
-//       var n = $('.js-part-table tr').length - 1;
-//       var newRow = $('<tr>' +
-//         '<td>' +
-//         '  <input name="part_letter_' + n + '">' +
-//         '</td>' +
-//         '<td>' +
-//         '  <input name="part_name_' + n + '">' +
-//         '</td>' +
-//         '<td>' +
-//         '  <input name="part_quantity_' + n + '" size="4">' +
-//         '</td>' +
-//         '<td>' +
-//         '  <input name="part_reason_' + n + '">' +
-//         '</td>' +
-//       '</tr>')
-//       $('.js-part-table').append(newRow);
-//       if (n == 15) {
-//         $('.js-add-rows').hide();
-//       }
-//     });
+$(function () {
+  if ($('.js-add-rows').length) {
 
-//     $('form.replacements').on('submit', function () {
-//       serializesomestuff
-//       http://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
-//     })
-//   }
-// });
+    function addRow() {
+      var n = $('.js-part-table tr').length - 1;
+      var newRow = $('<tr>' +
+        '<td>' +
+        '  <input name="parts[letter][' + n + ']" size="4">' +
+        '</td>' +
+        '<td>' +
+        '  <input name="parts[name][' + n + ']" size="25">' +
+        '</td>' +
+        '<td>' +
+        '  <input name="parts[quantity][' + n + ']" size="2">' +
+        '</td>' +
+        '<td>' +
+        '  <input name="parts[reason][' + n + ']" size="36">' +
+        '</td>' +
+      '</tr>')
+      $('.js-part-table').append(newRow);
+      if (n == 15) {
+        $('.js-add-rows').hide();
+      }
+    }
+
+    function preserveFormValues() {
+      partData = JSON.parse(JSON.parse($('.js-part-table').data('parts'))); // wow plz fix this encoding bug
+      if (partData) {
+        $.each(partData["letter"], function (k) {
+
+          while (k >= $('.js-part-table tr').length - 1) {
+            addRow();
+          }
+          $('input[name="parts\[letter\]\[' + k + '\]"]').val(partData["letter"][k])
+          $('input[name="parts\[name\]\[' + k + '\]"]').val(partData["name"][k])
+          $('input[name="parts\[quantity\]\[' + k + '\]"]').val(partData["quantity"][k])
+          $('input[name="parts\[reason\]\[' + k + '\]"]').val(partData["reason"][k])
+        })
+      }
+    }
+    $('.js-add-rows').on('click', function (e) { e.preventDefault(); addRow(); });
+
+    preserveFormValues();
+  }
+});
