beb9d5316dcdc882fd1136fac30d752c418fc440
Golf Sinteppadon <golf@twitch.tv>
2017-12-25 13:25:20 -0800

    carousel turbolinks multiple initialization bug

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,9 +1,13 @@
 $(function () {
-  var currentImageIndex = 0;
-  setInterval(carouselTransition, 3000);
+
+  let currentImageIndex = 0;
+
+  if (!window.carouselTimer) {
+    window.carouselTimer = setInterval(carouselTransition, 3000);
+  }
 
   function carouselTransition() {
-    var $images = $('.carousel img');
+    let $images = $('.carousel img');
     $images.css('opacity', '0');
     $images.eq(currentImageIndex % $images.length).css('opacity', '1');
     currentImageIndex++;
