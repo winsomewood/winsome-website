21f0ea0aea3a76fb021d8ec414f9dd16ff089d15
root <root@ubuntu.winsomewood.com>
2021-02-08 11:19:55 -0800

    Add read block when sending attachment to close file so can be saved to disk

--- app/mailers/info_mailer.rb
+++ app/mailers/info_mailer.rb
@@ -9,7 +9,7 @@ class InfoMailer < ApplicationMailer
   def replacement_email(replacement)
     @replacement = replacement
     if replacement.proof_of_purchase
-      attachments[replacement.proof_of_purchase.original_filename] = replacement.proof_of_purchase.read
+      attachments[replacement.proof_of_purchase.original_filename] = File.open(replacement.proof_of_purchase, 'rb') {|f| f.read}
     end
     mail(to: "replacement@winsomewood.com", subject: "[www.winsomewood.com] Request from #{replacement.name}")
   end
