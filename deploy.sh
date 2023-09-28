7a0f106653c1d8c13f531e411f5b993474c81d8f
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2023-09-28 16:58:19 -0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
 rsync -avz --exclude '.git/' --exclude 'node_modules/' --exclude '.bundle/' --exclude 'tmp/' . web@192.168.161.189:/var/www/
+ssh -t web@192.168.161.189 'sudo systemctl restart web'
+
+echo 'Deploy completed'
