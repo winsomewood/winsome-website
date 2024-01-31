2ae86b5c780baf87eb34a9612e688054bd7d0c83
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2024-01-31 15:21:42 -0500

    more winpub1 prep

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rsync -avz --exclude '.git/' --exclude 'node_modules/' --exclude '.bundle/' --exclude 'tmp/' . web@192.168.161.189:/var/www/
-ssh -t web@192.168.161.189 'sudo systemctl restart web'
+rsync -avz --exclude '.git/' --exclude 'node_modules/' --exclude '.bundle/' --exclude 'tmp/' . web@10.101.101.189:/var/www/
+ssh -t web@10.101.101.189 'sudo systemctl restart web'
 
 echo 'Deploy completed'
