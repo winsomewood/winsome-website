9253065a6e796ba8ac9bace159f57ea28ed844bc
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2024-04-08 22:38:21 -0700

    rsync should ignore public/images

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rsync -avz --exclude '.git/' --exclude 'node_modules/' --exclude '.bundle/' --exclude 'tmp/' . web@10.101.101.189:/var/www/
+rsync -avz --exclude 'public/images/' --exclude '.git/' --exclude 'node_modules/' --exclude '.bundle/' --exclude 'tmp/' . web@10.101.101.189:/var/www/
 ssh -t web@10.101.101.189 'sudo systemctl restart web'
 
 echo 'Deploy completed'
